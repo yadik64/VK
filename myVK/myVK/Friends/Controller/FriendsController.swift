d77e1b5130982ecee2840c419f4878b0c1d74cf9
yadik64 <yadik64@gmail.com>
2019-04-06 01:36:58 +0400

    [Добавил] - Поиск по друзьям

--- myVK/myVK/Friends/Controller/FriendsController.swift
+++ myVK/myVK/Friends/Controller/FriendsController.swift
@@ -9,11 +9,50 @@
 import UIKit
 
 class FriendsController: UIViewController {
+
+//TODO: - Outlets
     
     @IBOutlet weak var tableView: UITableView!
+
+//TODO: - Propertis
     
+    let searchController = UISearchController(searchResultsController: nil)
     let cellIdentifire = "FriendsViewCell"
     let segueIdentifire = "FotoAlbomSegue"
+    
+// FriendsController Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSearchController()
+    }
+    
+//TODO: - SearchController Methods
+    
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        searchController.searchBar.placeholder = "Поиск друга"
+        definesPresentationContext = true
+    }
+    
+    private func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    private func isFiltered() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    
+    private func filterContentForSearchText(searchText: String) {
+        FriendsModel.filterFriendsArray = FriendsModel.userFriendsArray.filter{ $0.nameFriend.lowercased().contains(searchText.lowercased())}
+        
+        tableView.reloadData()
+    }
+    
+//TODO: - Data transmission Methods
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -21,27 +60,46 @@ class FriendsController: UIViewController {
         guard let indexPath = tableView.indexPathForSelectedRow else { return }
         
         let friendAllFotoController = segue.destination as! FriendAllFotoController
+        let friendData = isFiltered() ?
+            FriendsModel.filterFriendsArray[indexPath.row] :
+            FriendsModel.userFriendsArray[indexPath.row]
         
-        friendAllFotoController.friendData = FriendsModel.userFriendsArray[indexPath.row]
+        friendAllFotoController.friendData = friendData
     }
 
 }
 
+//TODO: - Extensions
+
 extension FriendsController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return FriendsModel.userFriendsArray.count
+        guard isFiltered() else { return FriendsModel.userFriendsArray.count }
+        return FriendsModel.filterFriendsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifire, for: indexPath) as! FriendsViewCell
-        let avatar = UIImage(named: FriendsModel.userFriendsArray[indexPath.row].fotoFriend)
+        
+        let avatar = isFiltered() ?
+            UIImage(named: FriendsModel.filterFriendsArray[indexPath.row].fotoFriend) :
+            UIImage(named: FriendsModel.userFriendsArray[indexPath.row].fotoFriend)
+        let name = isFiltered() ?
+            FriendsModel.filterFriendsArray[indexPath.row].nameFriend :
+            FriendsModel.userFriendsArray[indexPath.row].nameFriend
+        
         cell.avatarView.image = avatar
-        cell.nameFriendLabel.text = FriendsModel.userFriendsArray[indexPath.row].nameFriend
+        cell.nameFriendLabel.text = name
         
         return cell
     }
     
+}
+
+extension FriendsController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
 }
